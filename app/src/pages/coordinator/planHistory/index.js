ac122b15d08ba36a37b34148c8859bc9aeedcceb
Abu Bakar <abubakar.80187@gmail.com>
2023-05-24 18:30:17 +0500

    1.0.0
    -- supervisor side
    	-- ideas not showing submitted by students
    
    -- cordinator side
    	-- plans pe tasks show ho rhy
    	-- cordinator ke plans ni show ho rhy
    	-- proposals ni show ho rhy
    
    -- hod side
    	-- dashboard pe projects ni show ho rhy
    	-- remarks tab main plans ni show ho rhy

--- app/src/pages/coordinator/planHistory/index.js
+++ app/src/pages/coordinator/planHistory/index.js
@@ -21,7 +21,7 @@ const PlanHistory = () => {
         <div className="sm:flex sm:items-center">
           <div className="sm:flex-auto">
             <h1 className="text-base font-semibold leading-6 text-gray-900">
-              Tasks-Submitted
+              Plans Submitted
             </h1>
           </div>
         
